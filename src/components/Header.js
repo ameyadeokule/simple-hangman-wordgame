87651b5a9cd6e14d2b593e4f39530ec06547ea80
Ameya Deokule <deokuleameya@gmail.com>
2021-03-16 19:16:59 +0530

    Completed the functionality of the game

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <>
-         <h1>Hangman</h1>
+         <h1 className='header'>Hangman</h1>
          <p>Find the hidden word - Enter the word</p>   
         </>
     )
